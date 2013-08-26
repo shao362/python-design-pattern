560d16db64fa4bd22bfa103eda56892b54859f17
david chen <shaochung@gmail.com>
2013-08-27 00:27:17 +0800

    add example to Mediator

--- Mediator.py
+++ Mediator.py
@@ -1,10 +1,73 @@
 ##################  design
+class Mediator(object):
+	def __init__(self):
+		self._memberDict = {}
+
+	def colleaqueChanged(colleaque):
+		pass
+
+	def addMember(self, name, colleaque):
+		self._memberDict[name] = colleaque
+		colleaque.setMediator(self)
+
+class Colleaque(object):
+	def setMediator(self, mediator):
+		self._mediator = mediator
+
+	def changed(self):
+		self._mediator.colleaqueChanged(self)
+
+	def getValue(self):
+		return None
+
+	def setValue(self, value):
+		pass
 
 ################## Concrete class
+class UIMediator(Mediator):
+	def colleaqueChanged(self, colleaque):
+		print colleaque.getValue()
+		print 'which one do you want me to pass the message?'
+
+class TextField(Colleaque):
+	def getValue(self):
+		return 'value from TextField'
+
+	def setValue(self):
+		pass
+
+class Button(Colleaque):
+	def getValue(self):
+		return 'value from Button'
+
+	def setValue(self):
+		pass
+
+class ListBox(Colleaque):
+	def getValue(self):
+		return 'value from ListBox'
+
+	def setValue(self):
+		pass
 
 ################## use case
 def main():
-    pass
+    mediator = UIMediator()
+
+    button = Button()
+
+    textField = TextField()
+
+    listBox = ListBox()
+
+    mediator.addMember('button', button)
+    mediator.addMember('textField', textField)
+    mediator.addMember('listBox', listBox)
+
+    button.changed()
+
+    listBox.changed()
+
 
 if __name__ == '__main__':
     main()
